07cb871c731477176d06c207d59e2f1552e36309
Nicola Bongiovanni <nikolaosbongio97@Air-di-Nicola.station>
2023-07-17 18:57:47 +0200

    Nothing

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -3,9 +3,9 @@
       <ul class="nav justify-content-center border-bottom pb-3 mb-3">
         <li class="nav-item"><a href="{{route('home')}}" class="nav-link px-2 text-white">Home</a></li>
         <li class="nav-item"><a href="{{route('product.index')}}" class="nav-link px-2 text-white">Annunci</a></li>
-       
       </ul>
+      
       <p class="text-center text-white">© 2023 Presto.it, DevSud Company</p>
-      <p class="text-center mb-0"><a href="{{route('revisor.become')}}" class="btn-revisor-footer rounded text-decoration-none">Diventa revisore</a></p>
+      <p class="text-center mb-0 start-text"><a href="{{route('revisor.become')}}" class="btn-revisor-footer rounded text-decoration-none">Diventa revisore</a></p>
     </footer>
   </div>
